84d0c9bb1b39b24a83cdb19732797a5c98a2ecfa
Truong Thanh Vinh <truongthanhvinh941@gmail.com>
2022-05-04 01:19:05 +0700

    Vinh implementation firebase

--- app/src/main/java/com/example/mobile_subproject_nhom05/LoginActivity.java
+++ app/src/main/java/com/example/mobile_subproject_nhom05/LoginActivity.java
@@ -1,14 +1,56 @@
 package com.example.mobile_subproject_nhom05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText Email = findViewById(R.id.edtMail);
+        EditText password = findViewById(R.id.edtPassword);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = Email.getText().toString().trim();
+                String passwordField = password.getText().toString().trim();
+
+
+                fAuth.signInWithEmailAndPassword(email, passwordField).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(LoginActivity.this, ListActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Error !!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mobile_subproject_nhom05/RegisterActivity.java
+++ app/src/main/java/com/example/mobile_subproject_nhom05/RegisterActivity.java
@@ -10,11 +10,60 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        Button btn_register = findViewById(R.id.btnRegister);
+        EditText edtName = findViewById(R.id.edtName);
+        EditText edtEmail = findViewById(R.id.edtMail);
+        EditText edtPass = findViewById(R.id.edtPassword);
+        EditText edtRePass = findViewById(R.id.edtTypePassword);
+
+        if(fAuth.getCurrentUser() != null) {
+
+        }
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edtName.getText().toString().trim();
+                String email = edtEmail.getText().toString().trim();
+                String pass = edtPass.getText().toString().trim();
+                String rePass = edtRePass.getText().toString().trim();
+
+                if(pass.equals(rePass)) {
+                    Toast.makeText(RegisterActivity.this, "SOS", Toast.LENGTH_SHORT).show();
+
+                    fAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "create success" , Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(i);
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "create Fail" , Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Pass không giống nhau", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
