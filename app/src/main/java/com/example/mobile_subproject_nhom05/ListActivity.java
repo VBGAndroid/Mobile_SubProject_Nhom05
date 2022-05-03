7b32b3c709f2b9c931383e977907bbf41d41a30c
Huỳnh Nguyễn Quốc Bảo <hnquocbao22@gmail.com>
2022-05-03 20:36:31 +0700

    Bao finish List + Details
    Bao finish List + Details

--- app/src/main/java/com/example/mobile_subproject_nhom05/ListActivity.java
+++ app/src/main/java/com/example/mobile_subproject_nhom05/ListActivity.java
@@ -9,7 +9,6 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_list);
     }
 }
