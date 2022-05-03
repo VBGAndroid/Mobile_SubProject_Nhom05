9ce263f1cc8fda035ab9126f7d177bd61a586bde
Truong Thanh Vinh <truongthanhvinh941@gmail.com>
2022-05-03 22:36:22 +0700

    Vinh finish screen login/register

---
+++ app/src/main/java/com/example/mobile_subproject_nhom05/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.mobile_subproject_nhom05;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
